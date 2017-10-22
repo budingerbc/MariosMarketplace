743423c7e63ff8484890e88963b195f0a3eee7a3
Ben Budinger <budingerbc@gmail.com>
2017-10-21 17:03:01 -0700

    Complete CRUD functionality for Product class in views and database.

--- MariosMarketplace/Controllers/ProductsController.cs
+++ MariosMarketplace/Controllers/ProductsController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MariosMarketplace.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Globalization;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,6 +19,54 @@ namespace MariosMarketplace.Controllers
         public IActionResult Index()
         {
             return View(db.Products.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisProduct = db.Products.Include(products => products.Reviews).FirstOrDefault(products => products.ProductId == id);
+            return View(thisProduct);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            return View(thisProduct);
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            return View(thisProduct);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            db.Products.Remove(thisProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- MariosMarketplace/Models/MariosMarketplaceContext.cs
+++ MariosMarketplace/Models/MariosMarketplaceContext.cs
@@ -8,11 +8,24 @@ namespace MariosMarketplace.Models
 {
     public class MariosMarketplaceContext : DbContext
     {
+        public MariosMarketplaceContext()
+        {
+        }
+
         public virtual DbSet<Product> Products { get; set; }
         public virtual DbSet<Review> Reviews { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder
                 .UseMySql(@"Server=localhost;Port=8889;database=marketplace;uid=root;pwd=root;");
+
+        public MariosMarketplaceContext(DbContextOptions<MariosMarketplaceContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
