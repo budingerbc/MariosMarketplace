c8438cea40125b1e31fbe8de59396b3ab400db69
Ben Budinger <budingerbc@gmail.com>
2017-10-23 20:51:36 -0700

    Start unit testing.

---
+++ MariosMarketplace.Tests/ControllerTests/ProductsControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MariosMarketplace.Models;
+using MariosMarketplace.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MariosMarketplace.Tests
+{
+    [TestClass]
+    public class ProductsControllerTest
+    {
+        [TestMethod]
+        public void GetDescriptionTest()
+        {
+            ProductsController controller = new ProductsController();
+
+            var result = controller.Index();
+
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
+
+        [TestMethod]
+        public void GetModelListIndexText()
+        {
+            ViewResult indexView = new ProductsController().Index() as ViewResult;
+
+            var result = indexView.ViewData.Model;
+
+            Assert.IsInstanceOfType(result, typeof(List<Product>));
+        }
+    }
+}
---
+++ MariosMarketplace.Tests/ModelTests/ProductTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MariosMarketplace.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
