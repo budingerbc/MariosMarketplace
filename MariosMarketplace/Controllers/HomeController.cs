987f08e3a242b0be46db81ee6db0d30643f207a5
Guest User <Guest@epicodus-79.local>
2017-10-26 17:23:43 -0700

    Create models in HomeController for recently added products and highest rated products.

--- MariosMarketplace/Controllers/HomeController.cs
+++ MariosMarketplace/Controllers/HomeController.cs
@@ -18,10 +18,14 @@ namespace MariosMarketplace.Controllers
 
         public IActionResult Index()
         {
-            var allProducts = db.Products.ToList().OrderByDescending(product => product.ProductId);
+            List<Product> recentlyAdded = db.Products.OrderByDescending(product => product.ProductId).Take(3).ToList();
+            List<Product> highlyRated = db.Products.Include(product => product.Reviews).OrderByDescending(review => review.CalculateAverageRating()).Take(3).ToList();
 
+            Dictionary<string, List<Product>> dict = new Dictionary<string, List<Product>>();
+            dict.Add("recent", recentlyAdded);
+            dict.Add("highlyRated", highlyRated);
 
-            return View(allProducts);
+            return View(dict);
         }
     }
 }
